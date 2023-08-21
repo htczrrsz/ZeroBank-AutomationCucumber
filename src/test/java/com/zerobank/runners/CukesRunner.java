50d3da85359f983f30896b7ed0b326455b098fdd
htczrrsz <hpzararsiz@gmail.com>
2023-08-21 13:03:36 -0500

    Initial commit

---
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/zerobank/step_defs",
+        plugin = {"json:target/cucumber.json"},
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+
+}
